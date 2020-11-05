59fb07e2c59a656cb1bbd52bdaf2972aafa4294e
Stoick01 <work.gordan.prastalo@gmail.com>
2020-11-05 20:06:17 +0100

    Added accuracy

--- examples/mnist.py
+++ examples/mnist.py
@@ -12,6 +12,7 @@ from bluebird.nn import NeuralNet
 from bluebird.activations import Relu, Sigmoid, Softmax, Tanh
 from bluebird.layers import Flatten, Dropout, Dense, Linear
 from bluebird.loss import CategoricalCrossEntropy
+from bluebird.metrics import accuracy
 
 (X_train, y_train), (X_test, y_test) = load_data()
 
@@ -47,3 +48,10 @@ for (im, tst) in zip(X_test[:10], y_test[:10]):
     pred = net.predict(np.array(im))
 
     print("{b: > 4}, {c: > 4}".format(b=np.argmax(pred), c=np.argmax(tst)))
+
+p = net.predict(X_test)
+
+p = p.argmax(axis=1)
+y_test = y_test.argmax(axis=1)
+
+print(accuracy(p, y_test))
